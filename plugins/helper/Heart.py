90ea7c4801fd683c69935930195fef624acd80ae
HeartXbotz <heartthief0801@gmail.com>
2025-01-10 12:47:43 +0530

    New

--- plugins/helper/Heart.py
+++ plugins/helper/Heart.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery 
-from plugins.helper.fonts import Fonts
+from plugins.helper.Heart import Fonts
 
 @Client.on_message(filters.private & filters.command(["font"]))
 async def style_buttons(c, m, cb=False):
